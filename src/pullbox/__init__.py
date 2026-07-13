8ec768290c9950d13265fbb17f54d47703e8d679
DeusExTaco <deus.ex.taco@gmail.com>
2026-07-13 01:31:55 -0700

    chore: bump version to 1.0.3 for release

--- src/pullbox/__init__.py
+++ src/pullbox/__init__.py
@@ -2,7 +2,7 @@
 
 from datetime import UTC, datetime
 
-__version__ = "1.0.3-dev"
+__version__ = "1.0.3"
 
 # Set once at process start; used by System > About for uptime calculation.
 STARTED_AT: datetime = datetime.now(UTC)
