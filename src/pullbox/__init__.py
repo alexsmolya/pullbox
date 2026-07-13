0120165b83616158b6a0f00321116f48b66735d3
DeusExTaco <deus.ex.taco@gmail.com>
2026-07-13 10:35:46 -0700

    chore: sync develop after v1.0.4 release

--- src/pullbox/__init__.py
+++ src/pullbox/__init__.py
@@ -2,7 +2,7 @@
 
 from datetime import UTC, datetime
 
-__version__ = "1.0.4"
+__version__ = "1.0.5-dev"
 
 # Set once at process start; used by System > About for uptime calculation.
 STARTED_AT: datetime = datetime.now(UTC)
